f91f3113381c533f59dcda24f7a4c68d42eac44b
Madinah2491 <madinah2491@gmail.com>
2018-05-19 09:53:28 -0400

    heroku deply

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,5 @@
 var mysql = require("mysql");
-var connection ;
-
-//basic connection to sql db
-// var connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'root',
-//     password: 'Maddough2018!',
-//     database: 'burgers_db'
-// })
+var connection;
 
     if (process.env.JAWSDB_URL){
         connection = mysql.createConnection(process.env.JAWSDB_URL);
