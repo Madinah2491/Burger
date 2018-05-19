4ff65360525d0bd7057d35b4e0138c8d18686727
Madinah2491 <madinah2491@gmail.com>
2018-05-19 09:45:26 -0400

    jaws db deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,26 @@
 var mysql = require("mysql");
+var connection ;
 
 //basic connection to sql db
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'Maddough2018!',
-    database: 'burgers_db'
-})
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: '3306',
+//     user: 'root',
+//     password: 'Maddough2018!',
+//     database: 'burgers_db'
+// })
 
-connection.connect(function(err){
-    if (err) throw err;
-    console.log("connected as ID" + connection.threadId);
-});
+    if (process.env.JAWSDB_URL){
+        connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else{
+        connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'Maddough2018!',
+            database: 'burgers_db'
+        });
+    };
+
+connection.connect();
 
 module.exports = connection;
